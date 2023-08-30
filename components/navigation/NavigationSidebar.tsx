4e0ddb0e04a20edf4dddd3b78a7edfa057fb364f
Blake Zajac <contact@blakezajac.com>
2023-08-30 14:58:37 +1000

    Render servers on sidebar

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,8 +1,11 @@
 import { redirect } from "next/navigation";
 
+import NavigationAction from "@/components/navigation/NavigationAction";
+import NavigationItem from "@/components/navigation/NavigationItem";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import NavigationAction from "@/components/navigation/NavigationAction";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,8 +26,22 @@ export const NavigationSidebar = async () => {
   });
 
   return (
-    <div className="flex flex-col items-center h-full space-y-4text-primary w-full dark:bg-[#1e1f22] py-3">
+    <div className="flex flex-col items-center h-full space-y-4 text-primary w-full dark:bg-[#1e1f22] py-3">
       <NavigationAction />
+
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+
+      <ScrollArea className="flex 1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
