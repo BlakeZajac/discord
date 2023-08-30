c3cee8fadc4902087a4fb9106116b5e31482177c
Blake Zajac <contact@blakezajac.com>
2023-08-30 13:51:40 +1000

    Add server image functionality

--- components/input/FileUpload.tsx
+++ components/input/FileUpload.tsx
@@ -18,7 +18,14 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
-        <Image fill src={value} alt="Uploaded Image" />
+        <Image fill src={value} alt="Uploaded Image" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     );
   }
