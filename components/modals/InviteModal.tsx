39eab80a6e826131b2dd75ef9e37fb574f2d6d48
Blake Zajac <contact@blakezajac.com>
2023-08-31 21:18:22 +1000

    Allow new link generation from the inviteModal

--- components/modals/InviteModal.tsx
+++ components/modals/InviteModal.tsx
@@ -13,17 +13,44 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-import { Copy, RefreshCcw } from "lucide-react";
+import { CheckIcon, Copy, RefreshCcw } from "lucide-react";
+import { useState } from "react";
+import axios from "axios";
 
 export const InviteModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
+  const { onOpen, isOpen, onClose, type, data } = useModal();
   const origin = useOrigin();
 
   const isModalOpen = isOpen && type === "invite";
   const { server } = data;
 
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-  console.log(server);
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  const onNewLink = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`
+      );
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+      console.log("Error generating link:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -41,16 +68,28 @@ export const InviteModal = () => {
 
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+              disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
               value={inviteUrl}
             />
 
-            <Button size="icon">
-              <Copy className="w-4 h-4" />
+            <Button
+              disabled={isLoading}
+              onClick={onCopy}
+              size="icon"
+              className={copied ? "!bg-emerald-500" : ""}
+            >
+              {copied ? (
+                <CheckIcon className="text-white w-4 h-4" />
+              ) : (
+                <Copy className="w-4 h-4" />
+              )}
             </Button>
           </div>
 
           <Button
+            disabled={isLoading}
+            onClick={onNewLink}
             variant="link"
             size="sm"
             className="text-xs text-zinc-500 mt-2 pl-0"
