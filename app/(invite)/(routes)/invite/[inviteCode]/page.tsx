42afbf6a726db7b18d7f199f14932719ad37d645
Blake Zajac <contact@blakezajac.com>
2023-08-31 21:31:08 +1000

    Create the invite code page

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,60 @@
+import { redirect } from "next/navigation";
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  // If we match the invite code of the server with that of the db, AND
+  // if we are already a member of that server, redirect the user to the server
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  // No need to assign a role here, they will be auto assigned as guest!
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+
+  return <div>Invite page</div>;
+};
+
+export default InviteCodePage;
