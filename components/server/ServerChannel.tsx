5f5201e8d377886db1fc826a10217a2f67fa9867
Blake Zajac <contact@blakezajac.com>
2023-09-03 12:17:21 +1000

    Add channel route functionality

--- components/server/ServerChannel.tsx
+++ components/server/ServerChannel.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { cn } from "@/lib/utils";
-import { useModal } from "@/hooks/useModalStore";
-
-import ActionTooltip from "@/components/common/ActionTooltip";
-
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { ActionTooltip } from "@/components/common/ActionTooltip";
+import { ModalType, useModal } from "@/hooks/useModalStore";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -22,8 +21,8 @@ const iconMap = {
 };
 
 export const ServerChannel = ({
-  server,
   channel,
+  server,
   role,
 }: ServerChannelProps) => {
   const { onOpen } = useModal();
@@ -32,15 +31,25 @@ export const ServerChannel = ({
 
   const Icon = iconMap[channel.type];
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
+
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
-        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10  dark:hover:bg-zinc-700/50 transition mb-1",
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
       )}
     >
-      <Icon className="flex-shrink-0 w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+      <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+
       <p
         className={cn(
           "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
@@ -55,24 +64,21 @@ export const ServerChannel = ({
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
             <Edit
-              onClick={() => onOpen("editChannel", { server, channel })}
-              className="hidden group-hover:block h-4 w-4 text-zinc-500 hover:text-zinc-600
-            dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+              onClick={(e) => onAction(e, "editChannel")}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
 
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() => onOpen("deleteChannel", { server, channel })}
-              className="hidden group-hover:block h-4 w-4 text-zinc-500 hover:text-zinc-600
-            dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+              onClick={(e) => onAction(e, "deleteChannel")}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
         </div>
       )}
-
       {channel.name === "general" && (
-        <Lock className="w-4 h-4 ml-auto text-zinc-500 dark:text-zinc-400" />
+        <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
       )}
     </button>
   );
