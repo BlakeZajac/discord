efe4e9e4cc4493619a574167b7742c35945f22ea
Blake Zajac <contact@blakezajac.com>
2023-09-03 13:57:27 +1000

    Set up web socket API

--- types.ts
+++ types.ts
@@ -1,5 +1,17 @@
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIoServer } from "socket.io";
+
 import { Member, Server, Profile } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIoServer;
+    };
+  };
 };
