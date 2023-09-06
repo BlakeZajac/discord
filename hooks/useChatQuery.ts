4e3bcdc2686007ac460affcf98c1775044572573
Blake Zajac <contact@blakezajac.com>
2023-09-06 18:31:46 +1000

    Create useChatQuery

---
+++ hooks/useChatQuery.ts
@@ -0,0 +1,45 @@
+import { useParams } from "next/navigation";
+
+import qs from "query-string";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { useSocket } from "@/components/providers/SocketProvider";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+
+export const useChatQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) => {
+  const { isConnected } = useSocket();
+  const params = useParams();
+
+  const fetchMessages = async ({ pageParam = undefined }) => {
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          cusror: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: [queryKey],
+      queryFn: fetchMessages,
+    });
+};
