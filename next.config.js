b0b32c43439c006f620a00a5a11abcab711c000d
Blake Zajac <contact@blakezajac.com>
2023-09-07 23:26:09 +1000

    Fix: websocket errors, downgrade nextjs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+            "utf-8-validate": "commonjs utf-8-validate",
+            bufferutiil: "commonjs bufferutil"
+        })
+    },
+
     images: {
         remotePatterns: [
             {
